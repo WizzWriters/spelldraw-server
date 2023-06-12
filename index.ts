f20ff682eb390a6988d9c2794c526d0bbf43abca
MusicFreak456 <czarek44564@gmail.com>
2023-06-12 22:39:47 +0200

    Setup hot reloads

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import 'dotenv/config'
 
 const app = express()
 const port = process.env.PORT
+const domain = process.env.DOMAIN
 
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send('Hi!')
 })
 
 app.listen(port, () => {
-  console.log(`⚡Server is running at http://localhost:${port}`);
+  console.log(`⚡Server is running on ${domain}:${port}`);
 });
