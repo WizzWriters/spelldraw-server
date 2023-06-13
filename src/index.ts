42cbdcd77be496c20b7913afded8ece3434aa186
MusicFreak456 <czarek44564@gmail.com>
2023-06-13 03:07:11 +0200

    Install socket.io and listen for connections

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import express from 'express'
 import 'dotenv/config'
+import { Server } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
 
-const app = express()
 const port = process.env.PORT
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`)
+})
 
 app.get('/', (req, res) => {
   res.send('Hi!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`⚡Server is listening on port ${port}`)
 })
